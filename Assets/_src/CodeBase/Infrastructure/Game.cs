d87ac7b763fd5ea498173893e505d797404351ab
Artem <atyom.najdanov@mail.ru>
2022-06-07 23:18:48 +0500

    added RegisterInputService for Game, implemented IInputService, implemented InputService, implemented MobileInputService, implemented StandaloneInputService

--- Assets/_src/CodeBase/Infrastructure/Game.cs
+++ Assets/_src/CodeBase/Infrastructure/Game.cs
@@ -1,7 +1,24 @@
-﻿namespace _src.CodeBase.Infrastructure
+﻿using _src.CodeBase.Services.Input;
+using UnityEngine;
+
+namespace _src.CodeBase.Infrastructure
 {
     public class Game
     {
-        
+        public static IInputService InputService;
+
+        public Game()
+        {
+            RegisterInputService();
+        }
+
+
+        private void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputService = new StandaloneInputService();
+            else
+                InputService = new MobileInputService();
+        }
     }
 }
---
+++ Assets/_src/CodeBase/Services/Input/MobileInputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _src.CodeBase.Services.Input
+{
+    public class MobileInputService : InputService
+    {
+        public override Vector2 Axis => SimpleInputAxis();
+    }
+}
